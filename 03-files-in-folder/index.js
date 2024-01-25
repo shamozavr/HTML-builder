ad07193051f104f05326ca454b74ab9b20e6423f
Roman Shashin <propeller1992@gmail.com>
2024-01-25 13:18:57 +0300

    fix: 03-task Sync removed

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -9,16 +9,20 @@ fs.readdir(path.join(__dirname, '/secret-folder'), (err, files) => {
     return;
   }
   files.forEach((file) => {
-    let stats = fs.statSync(path.join(__dirname, `/secret-folder/${file}`));
-    let fileSize = stats.size;
-    let fileName = `${path.parse(`${file}`).name}`;
-    let fileExt = `${path.extname(`${file}`)}`;
-    if (stats.isFile()) {
-      stdout.write(
-        `${fileName} - 
-        ${fileExt.slice(1)} - 
-        ${(fileSize / 1024).toFixed(3)}kb\n`,
-      );
-    }
+    fs.stat(path.join(__dirname, `/secret-folder/${file}`), (err, stats) => {
+      if (err) {
+        console.error(err);
+      }
+      let fileSize = stats.size;
+      let fileName = `${path.parse(`${file}`).name}`;
+      let fileExt = `${path.extname(`${file}`)}`;
+      if (stats.isFile()) {
+        stdout.write(
+          `${fileName} - ${fileExt.slice(1)} - ${(fileSize / 1024).toFixed(
+            3,
+          )}kb\n`,
+        );
+      }
+    });
   });
 });
